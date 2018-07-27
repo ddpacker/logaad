a76f140e798cce6afa47d110489aedde1d546dfd
Andres Munoz <and54@hotmail.com>
2018-07-27 16:34:30 -0400

    portfoliovalue added

--- client/src/services/PortfolioValue.js
+++ client/src/services/PortfolioValue.js
@@ -1,28 +1,51 @@
 import EventBus from "./EventBus";
 
-class Tickers { 
-    static suscribeTicker(stock, time, callBack){
-        if(!this.tickers)this.tickers = {};
-        if(!this.tickers[stock+time])this.createTicker(stock, time);
-        EventBus.getEventEmitter().on(stock+"_"+time, callBack);
-        return "";
-    }
-
-    static createTicker(stock, time){
-        this.tickers[stock+time] = {
-            inte: setInterval(this.bringStock, {month:43200000, day:300000}[time], stock, time)
+class PortfolioValue { 
+    static portfolio;
+    static suscribe(stocks, callBack){
+        EventBus.getEventEmitter().on("portfolio", callBack);
+        if(this.portfolio)clearInterval(this.portfolio.inte);
+        this.portfolio = {
+            inte: setInterval(this.bringPortfolio.bind(this), 60000, stocks),
+            stocks: stocks
         };
-        this.bringStock(stock, time);
+        this.bringPortfolio(stocks);
     }
 
-    static bringStock(stock, time){
-        const path = "https://api.iextrading.com/1.0/stock/";
-        const extras = "/batch?types=quote,chart&range="+{day:"1d&chartInterval=5", month:"1m"}[time];
-        fetch(path + stock + extras, {method: "get"}).then(function(response){
+    static bringPortfolio(stocks){
+        let symbols = "";
+        for (let i in stocks){
+            symbols += (symbols?",":"")+i;
+        }
+        const path = "https://api.iextrading.com/1.0/stock/market/batch?symbols="+symbols+"&types=chart&range=1d&last=1&chartLast=5";
+        fetch(path, {method: "get"}).then(function(response){
             response.json().then(function(data) {
-                EventBus.eventEmitter.emit(stock+"_"+time, {id:stock+"_"+time, data:data});
+                let obj = {
+                    totalEquity: 0, 
+                    tickers: {}
+                };
+                console.log(this.portfolio);
+                for (let i in data){
+                    if(data[i] && data[i].chart && data[i].chart.length){
+                        const stock = i.toLowerCase();
+                        const chart = this.verifyData(data[i].chart, 4);
+                        const stockEquity = chart.average * stocks[stock];
+                        obj.tickers[stock] = {
+                            tickerValue: Math.round(chart.average*100)/100, 
+                            stockEquity: Math.round(stockEquity*100)/100
+                        };
+                        obj.totalEquity += stockEquity;
+                    }
+                }  
+                obj.totalEquity = Math.round(obj.totalEquity*100)/100;
+                EventBus.eventEmitter.emit("portfolio", obj);
             }.bind(this));
         }.bind(this));
     }
+
+    static verifyData(data, num) {
+        if(data[num].average>0 || !num)return data[num];
+        else return this.verifyData(data, num-1);
+    }
 }
-export default Tickers;
+export default PortfolioValue;

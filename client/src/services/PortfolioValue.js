5fd7a82135c68875dc6bdf3d1250fb235ca545f0
Andres Munoz <and54@hotmail.com>
2018-07-31 13:34:52 -0400

    chart color changes and validation of initial price

--- client/src/services/PortfolioValue.js
+++ client/src/services/PortfolioValue.js
@@ -4,7 +4,7 @@ class PortfolioValue {
     static portfolio;
     static suscribe(stocks, callBack){
         EventBus.getEventEmitter().on("portfolio", callBack);
-        if(this.portfolio)clearInterval(this.portfolio.inte);
+        if (this.portfolio)clearInterval(this.portfolio.inte);
         this.portfolio = {
             inte: setInterval(this.bringPortfolio.bind(this), 60000, stocks),
             stocks: stocks
@@ -22,37 +22,37 @@ class PortfolioValue {
             response.json().then(function(data) {
                 let obj = {
                     totalEquity: 0, 
-                    tickers: [],//{}
+                    tickers: [],
                     totalChange: 0
 
                 };
-                var unweightedChange = 0;
-                var totalShares = 0;
+                let unweightedChange = 0;
+                let totalShares = 0;
                 for (let i in data){
                     if(data[i] && data[i].chart && data[i].chart.length){
                         const stock = i.toLowerCase();
                         const chart = this.verifyData(data[i].chart, 4);
                         const open = data[i].ohlc.open.price;
-                        const current = Math.round(chart.average*100)/100;
+                        const current = chart.average.toFixed(2);
                         const stockEquity = chart.average * stocks[stock];
-                        const stockUnweighted = (((current - open) / open) * 100)
-                        const stockChange = stockUnweighted * stocks[stock]
-                        const ticker = /*obj.tickers[stock] = */{
+                        const stockUnweighted = (((current - open) / open) * 100);
+                        const stockChange = stockUnweighted * stocks[stock];
+                        const ticker = {
                             tickerName: stock,
                             tickerValue: current,
-                            stockEquity: Math.round(stockEquity*100)/100,
+                            stockEquity: stockEquity.toFixed(2),
                             shares: stocks[stock],
                             percentChange: stockUnweighted.toFixed(3)
                         };
                         obj.tickers.push(ticker);
                         obj.totalEquity += stockEquity;
-                        unweightedChange += +stockChange.toFixed(3);
-                        totalShares += stocks[stock]
+                        unweightedChange += Number(stockChange.toFixed(3));
+                        totalShares += stocks[stock];
                     }
                 }
                 obj.totalChange = (unweightedChange / totalShares).toFixed(3);
                 obj.tickers.sort(this.compare);
-                obj.totalEquity = Math.round(obj.totalEquity*100)/100;
+                obj.totalEquity = obj.totalEquity.toFixed(2);
                 EventBus.eventEmitter.emit("portfolio", obj);
             }.bind(this));
         }.bind(this));
@@ -64,7 +64,7 @@ class PortfolioValue {
     }
 
     static verifyData(data, num) {
-        if(data[num].average>0 || !num)return data[num];
+        if (data[num].average>0 || !num)return data[num];
         else return this.verifyData(data, num-1);
     }
 }
--- client/src/services/Tickers.js
+++ client/src/services/Tickers.js
@@ -23,7 +23,7 @@ class Tickers {
     }
     static bringStock(stock, time){
         const path = "https://api.iextrading.com/1.0/stock/";
-        const extras = "/batch?types=quote,chart&range="+{day:"1d&chartInterval=5", month:"1m"}[time];
+        const extras = "/batch?types=quote,chart,ohlc&range="+{day:"1d&chartInterval=5", month:"1m"}[time];
         fetch(path + stock + extras, {method: "get"}).then(function(response){
             response.json().then(function(data) {
                 EventBus.eventEmitter.emit(stock+"_"+time, {id:stock+"_"+time, data:data});
